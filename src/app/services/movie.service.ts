2d9eae5475520d70b4c34240ee9706a6904cdc37
deficts <diegoar7@live.com>
2020-05-24 21:31:49 -0500

    Eliminando pagina de home y creando servicio de peliculas

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+export enum SearchType{
+  all='',
+  movie='movie',
+  series='series',
+  episode='episode'
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
+  url='http://www.omdbapi.com/';
+  apiKey='22783d6';
+  constructor(private http: HttpClient) {   }
 
-  constructor() { }
+  searchData(title:string, type:SearchType): Observable<any>{
+    return this.http.get(`${this.url}?s=${encodeURI(title)}&type=${type}&apikey=${this.apiKey}`).pipe(
+      map(results=>{
+        console.log('Raw',results);
+        return results['Search'];
+      })
+    );
+  }
+
+  getDetails(id){
+    return this.http.get(`${this.url}?i=${id}&plot=full&apikey=${this.apiKey}`);
+  }
 }
+
